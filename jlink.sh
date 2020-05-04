91182087832ceba7fc11d197c2d023e3a44bf37b
Mateusz Tomaszkiewicz <ressetpublic@gmail.com>
2020-05-04 21:29:28 +0200

    Remove relative path from JLinkGDBServer startup script

--- jlink.sh
+++ jlink.sh
@@ -1,1 +1,3 @@
-~/root/opt/JLink_Linux_V662d_x86_64/JLinkGDBServer -device FE310 -if JTAG -speed 4000 -port 2331 -nogui
+#!/bin/sh
+
+JLinkGDBServer -device FE310 -if JTAG -speed 4000 -port 2331 -nogui
