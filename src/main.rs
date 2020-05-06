0865f82ece87c9e1ffea49029af4005b97eb874b
Mateusz Tomaszkiewicz <ressetpublic@gmail.com>
2020-05-06 22:23:40 +0200

    Send UART hello message on start

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,27 @@ extern crate panic_halt;
 use riscv_rt::entry;
 use hifive1::hal::prelude::*;
 use hifive1::hal::DeviceResources;
+use hifive1::{pin, sprintln};
 
 #[entry]
 fn main() -> ! {
     let dr = DeviceResources::take().unwrap();
     let p = dr.peripherals;
+    let pins = dr.pins;
 
     // Configure clocks
-    let _clocks = hifive1::clock::configure(p.PRCI, p.AONCLK, 320.mhz().into());
+    let clocks = hifive1::clock::configure(p.PRCI, p.AONCLK, 320.mhz().into());
+
+    // Configure UART for stdout
+    hifive1::stdout::configure(
+        p.UART0,
+        pin!(pins, uart0_tx),
+        pin!(pins, uart0_rx),
+        115_200.bps(),
+        clocks,
+    );
+
+    sprintln!("hIMUdev app v0.1.0");
 
     loop {}
 }
